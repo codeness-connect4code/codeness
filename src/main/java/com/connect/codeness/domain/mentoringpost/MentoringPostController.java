716df30dd78f0cc70d43b0c2233b22eb9ddb2497
j-hann <j.han77055@gmail.com>
2025-01-08 20:53:17 +0900

    ✨ Feat : 멘터링 공고 생성 API 구현
    멘터링 공고 생성 API 구현

--- src/main/java/com/connect/codeness/domain/mentoringpost/MentoringPostController.java
+++ src/main/java/com/connect/codeness/domain/mentoringpost/MentoringPostController.java
@@ -1,10 +1,34 @@
 package com.connect.codeness.domain.mentoringpost;
 
+import com.connect.codeness.domain.mentoringpost.dto.MentoringPostCreateRequestDto;
+import com.connect.codeness.global.dto.CommonResponseDto;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/mentoring")
 public class MentoringPostController {
 
+	private final MentoringPostService mentoringPostService;
+
+	public MentoringPostController(MentoringPostService mentoringPostService) {
+		this.mentoringPostService = mentoringPostService;
+	}
+
+	/**
+	 * 멘토링 공고 생성 API
+	 */
+	@PostMapping
+	public ResponseEntity<CommonResponseDto> createMentoringPost(
+		@Valid @RequestBody MentoringPostCreateRequestDto requestDto) {
+
+		CommonResponseDto responseDto = mentoringPostService.createMentoringPost(1L, requestDto);
+
+		return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+	}
 }
--- src/main/java/com/connect/codeness/domain/mentoringpost/dto/MentoringPostCreateRequestDto.java
+++ src/main/java/com/connect/codeness/domain/mentoringpost/dto/MentoringPostCreateRequestDto.java
@@ -1,40 +1,35 @@
 package com.connect.codeness.domain.mentoringpost.dto;
 
 
-import com.connect.codeness.domain.user.User;
 import com.connect.codeness.global.enums.FieldType;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import lombok.Builder;
 import lombok.Getter;
 
-/**
- * TODO : 임시로 만들어 둔 dto, 필요 없을 시 삭제 예정
- */
+@Builder
 @Getter
-public class MentoringPostResponseDto {
-
-	@NotBlank
-	private Long id;//멘토링 공고 고유 식별자
-
-	@NotBlank
-	private User user;//사용자 고유 식별자 (외래키)
+public class MentoringPostCreateRequestDto {
 
 	@NotNull
 	private FieldType field; //분야
 
 	@NotNull
+	@Size(max = 30, message = "공고 제목은 {max}자 이내로 작성이 가능합니다.")
 	private String title;//공고 제목
 
 	@NotNull
+	@Size(max = 20, message = "회사 이름은 {max}자 이내로 작성이 가능합니다.")
 	private String company;//회사 이름
 
 	@NotNull
 	private Integer career;//경력
 
 	@NotNull
+	@Size(max = 30, message = "지역은 {max}자 이내로 작성이 가능합니다.")
 	private String region;//지역
 
 	@NotNull
@@ -53,5 +48,7 @@ public class MentoringPostResponseDto {
 	private LocalTime endTime;//멘토링 마감 시간
 
 	@NotNull
+	@Size(max = 300, message = "설명글은 {max}자 이내로 작성이 가능합니다.")
 	private String description;//설명글
+
 }
