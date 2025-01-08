f09f5a3272c6e18609be24701fc96af1f06f31f2
parkmookeun <sohappynow12@naver.com>
2025-01-08 14:45:31 +0900

    :fire: Remove: 리뷰 컨트롤러 코드 삭제

--- src/main/java/com/connect/codeness/domain/review/ReviewController.java
+++ src/main/java/com/connect/codeness/domain/review/ReviewController.java
@@ -25,7 +25,7 @@ public class ReviewController {
     ){
 
         CommonResponseDto commonResponseDto = reviewService.createReview(paymentListId, dto);
-        hihihihihi5678
+
         return new ResponseEntity<>(commonResponseDto, HttpStatus.CREATED);
     }
 }
