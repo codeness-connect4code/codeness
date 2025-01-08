43bc45624533cb15a4bfa96092f41415742a8f5b
박예진 <qkrdpwls2002>
2025-01-08 14:29:02 +0900

    :sparkles: Feat : 멘토링 공고 생성 컨트롤러 수정

--- src/main/java/com/connect/codeness/domain/mentoringpost/MentoringPostController.java
+++ src/main/java/com/connect/codeness/domain/mentoringpost/MentoringPostController.java
@@ -1,10 +1,28 @@
 package com.connect.codeness.domain.mentoringpost;
 
+import com.connect.codeness.global.dto.CommonResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class MentoringPostController {
+
+	private final MentoringPostService mentoringPostService;
+
+	public MentoringPostController(MentoringPostService mentoringPostService) {
+		this.mentoringPostService = mentoringPostService;
+	}
+
+	//todo : 파일 service 구현시 파일 추가로 수정 (사원증 이미지 첨부)
+	@PostMapping("/mentors")
+	public ResponseEntity<CommonResponseDto> createMentoringPost(
+		@RequestBody MentoringPostCreateDto mentoringPostCreateDto
+	) {}
+
+	
 
 }
