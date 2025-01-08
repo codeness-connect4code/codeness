812a95820b8d08bc42042ecc7af4ac71efeddc8a
Vaxee03 <012jas84@gmail.com>
2025-01-08 17:33:20 +0900

    :bug: Fix : 게시글 제목 글자수 제한 추가

--- src/main/java/com/connect/codeness/domain/post/dto/PostCreateRequestDto.java
+++ src/main/java/com/connect/codeness/domain/post/dto/PostCreateRequestDto.java
@@ -2,6 +2,7 @@ package com.connect.codeness.domain.post.dto;
 
 import com.connect.codeness.global.enums.PostType;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,6 +11,7 @@ import lombok.Getter;
 public class PostCreateRequestDto {
 
 	@NotBlank(message = "제목을 입력해주세요.")
+	@Pattern(regexp = "^.{0,50}$", message = "제목은 최대 50자 입니다.")
 	private String title;
 
 	@NotBlank(message = "내용은 비워둘 수 없습니다.")
