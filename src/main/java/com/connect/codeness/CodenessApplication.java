547b402fa1c8569002dec3dd02c08792a0972cfd
parkmookeun <sohappynow12@naver.com>
2025-01-08 20:24:57 +0900

    :sparkles: Feat : 리뷰 조회 API 구현

--- src/main/java/com/connect/codeness/CodenessApplication.java
+++ src/main/java/com/connect/codeness/CodenessApplication.java
@@ -2,12 +2,13 @@ package com.connect.codeness;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CodenessApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodenessApplication.class, args);
 	}
-
 }
