25ced468e83dac9145dc4046837b0b7bc5470fb3
Vaxee03 <012jas84@gmail.com>
2025-01-08 22:12:18 +0900

    ✨ Feat : 댓글 생성 API 구현
    - 댓글 생성 API 구현
    
    - 댓글 생성 dto 글자수 제한 방식 수정

--- src/main/java/com/connect/codeness/domain/comment/CommentServiceImpl.java
+++ src/main/java/com/connect/codeness/domain/comment/CommentServiceImpl.java
@@ -1,9 +1,40 @@
 package com.connect.codeness.domain.comment;
 
+import com.connect.codeness.domain.comment.dto.CommentCreateRequestDto;
+import com.connect.codeness.domain.post.Post;
+import com.connect.codeness.domain.post.PostRepository;
+import com.connect.codeness.global.dto.CommonResponseDto;
+import com.connect.codeness.global.enums.CommunityStatus;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentServiceImpl implements CommentService {
+
+	private final PostRepository postRepository;
+	private CommentRepository commentRepository;
+
+	public CommentServiceImpl(CommentRepository commentRepository, PostRepository postRepository) {
+		this.commentRepository = commentRepository;
+		this.postRepository = postRepository;
+	}
+
+	@Override
+	public CommonResponseDto createComment(Long postId, CommentCreateRequestDto dto){
+
+		Post post = postRepository.findByIdOrElseThrow(postId);
+
+		Comment comment = new Comment().builder()
+			.post(post)
+			.content(dto.getContent())
+			.status(CommunityStatus.DISPLAYED)
+			.build();
+
+		commentRepository.save(comment);
+
+		return CommonResponseDto.builder()
+			.msg("댓글 작성이 완료되었습니다.")
+			.build();
+	}
 
 }
 
