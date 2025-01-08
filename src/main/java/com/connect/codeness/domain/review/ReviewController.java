3c357f1829dd18dee8a50443b0a02a3fedef1f0c
parkmookeun <sohappynow12@naver.com>
2025-01-08 14:34:28 +0900

    :bug: bug: hi 버그 발견4

--- src/main/java/com/connect/codeness/domain/review/ReviewController.java
+++ src/main/java/com/connect/codeness/domain/review/ReviewController.java
@@ -25,7 +25,7 @@ public class ReviewController {
     ){
 
         CommonResponseDto commonResponseDto = reviewService.createReview(paymentListId, dto);
-        hihihihi
+        hihihihihi
         return new ResponseEntity<>(commonResponseDto, HttpStatus.CREATED);
     }
 }
