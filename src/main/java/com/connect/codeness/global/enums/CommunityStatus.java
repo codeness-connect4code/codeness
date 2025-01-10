e2fca2e00f150c5bd833c7755ded61594d895fe4
Vaxee03 <012jas84@gmail.com>
2025-01-10 12:33:26 +0900

    :recycle: Refactor : Enum 변수 및 생성자 추가
    Enum 변수 및 생성자 추가
    comment 클래스 @Enumerated 추가
    post / PostCreateRequestDto 글자 수 제한 코드 변경
    PostController createPost 메서드 @Valid 추가

--- src/main/java/com/connect/codeness/global/enums/CommunityStatus.java
+++ src/main/java/com/connect/codeness/global/enums/CommunityStatus.java
@@ -1,5 +1,12 @@
 package com.connect.codeness.global.enums;
 
 public enum CommunityStatus {
-	DELETED, DISPLAYED;
+	DELETED("삭제"),
+	DISPLAYED("존재");
+
+	private final String CommunityStatusText;
+
+	CommunityStatus(String CommunityStatusText) {
+		this.CommunityStatusText = CommunityStatusText;
+	}
 }
