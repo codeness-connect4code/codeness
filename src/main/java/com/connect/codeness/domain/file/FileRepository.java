75e3de0d6164faa65bd3c53a9d8df0f104f468e9
Vaxee03 <012jas84@gmail.com>
2025-01-13 23:45:04 +0900

    :sparkles: Feat : 게시글 상세 조회 API 구현
    - 게시글 상세 조회 API 구현
    - 파일 업로드 로직 리턴값 수정 ( 객체 → URL )

--- src/main/java/com/connect/codeness/domain/file/FileRepository.java
+++ src/main/java/com/connect/codeness/domain/file/FileRepository.java
@@ -1,7 +1,8 @@
 package com.connect.codeness.domain.file;
 
 import com.connect.codeness.global.enums.FileCategory;
-import java.util.NoSuchElementException;
+import com.connect.codeness.global.exception.BusinessException;
+import com.connect.codeness.global.exception.ExceptionType;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,13 +10,15 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface FileRepository extends JpaRepository<ImageFile, Long> {
 
+	Optional<ImageFile> findByUserId(Long userId);
+
 	// fileCategory와 userId로 파일 목록 검색
 	Optional<ImageFile> findByUserIdAndFileCategory(Long userId, FileCategory fileCategory);
 
 	// fileCategory와 userId로 파일 찾기 (없으면 예외 던짐)
 	default ImageFile findByUserIdAndFileCategoryOrElseThrow(Long userId, FileCategory fileCategory) {
 		return findByUserIdAndFileCategory(userId, fileCategory)
-			.orElseThrow(() -> new NoSuchElementException("No file found with fileCategory: " + fileCategory + " and userId: " + userId));
+			.orElseThrow(() -> new BusinessException(ExceptionType.NOT_FOUND_FILE));
 	}
 
 
