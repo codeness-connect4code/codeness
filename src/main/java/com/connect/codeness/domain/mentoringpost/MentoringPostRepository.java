1cc7e6af718b48d54c02fd2b3cd46b99db0da76b
박예진 <qkrdpwls2002>
2025-01-14 17:48:38 +0900

    🐛 Fix : 멘토링 추천 API 버그 수정

--- src/main/java/com/connect/codeness/domain/mentoringpost/MentoringPostRepository.java
+++ src/main/java/com/connect/codeness/domain/mentoringpost/MentoringPostRepository.java
@@ -13,18 +13,23 @@ import org.springframework.stereotype.Repository;
 public interface MentoringPostRepository extends JpaRepository<MentoringPost, Long> {
 
 	@Query(
-		"SELECT new com.connect.codeness.domain.mentoringpost.dto.MentoringPostRecommendResponseDto(m, " +
+		value = "SELECT new com.connect.codeness.domain.mentoringpost.dto.MentoringPostRecommendResponseDto(m, " +
 			"COALESCE((SELECT CAST(AVG(r.starRating) AS DOUBLE) FROM Review r " +
-			"WHERE r.paymentList.payment.mentoringSchedule.mentoringPost.id = m.id), 0.0)) " + // `CAST`로 `Double`로 변환
-			"FROM MentoringPost AS m " +
+			"JOIN r.paymentList pl " +
+			"JOIN pl.payment p " +
+			"JOIN p.mentoringSchedule ms " +
+			"WHERE ms.mentoringPost.id = m.id), 0.0)) " +
+			"FROM MentoringPost m " +
 			"WHERE (:field IS NULL OR m.field = :field) " +
-			"AND (:region IS NULL OR m.region LIKE %:region%)"
+			"AND (:region IS NULL OR m.region LIKE %:region% OR :region IS NULL)"
 	)
 	Page<MentoringPostRecommendResponseDto> findByFilter(
 		@Param("field") FieldType field,
 		@Param("region") String region,
 		Pageable pageable
 	);
+
+
 
 
 }
