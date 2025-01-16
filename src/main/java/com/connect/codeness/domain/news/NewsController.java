44dcb4567b6c8644adf455aa2b1c73503805c7d3
parkmookeun <94534687+parkmookeun@users.noreply.github.com>
2025-01-16 21:34:16 +0900

    ♻ Refactor : 페이지 객체 커스텀 처리
    ♻ Refactor : 페이지 객체 커스텀 처리

--- src/main/java/com/connect/codeness/domain/news/NewsController.java
+++ src/main/java/com/connect/codeness/domain/news/NewsController.java
@@ -3,9 +3,9 @@ package com.connect.codeness.domain.news;
 import static com.connect.codeness.global.constants.Constants.PAGE_NUMBER;
 import static com.connect.codeness.global.constants.Constants.PAGE_SIZE;
 
+import com.connect.codeness.domain.mentoringpost.dto.PaginationResponseDto;
 import com.connect.codeness.domain.news.dto.NewsResponseDto;
 import com.connect.codeness.global.dto.CommonResponseDto;
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,11 +24,11 @@ public class NewsController {
 	}
 
 	@GetMapping
-	public ResponseEntity<CommonResponseDto<Page<NewsResponseDto>>> findNews(
+	public ResponseEntity<CommonResponseDto<PaginationResponseDto<NewsResponseDto>>> findNews(
 		@RequestParam(defaultValue = PAGE_NUMBER) int pageNumber,
 		@RequestParam(defaultValue = PAGE_SIZE) int pageSize
 	) {
-		CommonResponseDto<Page<NewsResponseDto>> responseDto
+		CommonResponseDto<PaginationResponseDto<NewsResponseDto>> responseDto
 			= newsService.findNews(pageNumber, pageSize);
 
 		return new ResponseEntity<>(responseDto, HttpStatus.OK);
