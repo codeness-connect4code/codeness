37d825c9d3242c4bcbe235d5934fd9dc6fca680b
박예진 <qkrdpwls2002>
2025-01-27 12:00:57 +0900

    🔀 Branch : 병합

--- src/main/java/com/connect/codeness/domain/admin/dto/AdminSettlementListResponseDto.java
+++ src/main/java/com/connect/codeness/domain/admin/dto/AdminSettlementListResponseDto.java
@@ -12,12 +12,18 @@ public class AdminSettlementListResponseDto {
 	private Long count;
 	private BigDecimal totalCost;
 	private LocalDateTime createAt;
+	private String account;
+	private BigDecimal charge;
+	private BigDecimal finalCost;
 
-	public AdminSettlementListResponseDto(Long mentorId, String mentorName, Long count, BigDecimal totalCost, LocalDateTime createAt) {
+	public AdminSettlementListResponseDto(Long mentorId, String mentorName, Long count, BigDecimal totalCost, LocalDateTime createAt, String account) {
 		this.mentorId = mentorId;
 		this.mentorName = mentorName;
 		this.count = count;
 		this.totalCost = totalCost;
 		this.createAt = createAt;
+		this.account = account;
+		this.charge = totalCost.multiply(BigDecimal.valueOf(0.04));
+		this.finalCost = totalCost.subtract(this.charge);
 	}
 }
