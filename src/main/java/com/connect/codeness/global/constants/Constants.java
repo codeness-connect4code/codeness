a0261f5a807d1a91b9f02601662ee07ff0be2aa6
parkmookeun <sohappynow12@naver.com>
2025-01-28 15:58:41 +0900

    ♻️ Refactor : 채팅방 생성 로직 수정
    chatRoomHistory에 expireAt 컬럼 추가(채팅방 만료일) / 채팅방 생성 로직에 생성(중복 생성 포함)시, 스케줄 시간을 기반으로 만료일 수정

--- src/main/java/com/connect/codeness/global/constants/Constants.java
+++ src/main/java/com/connect/codeness/global/constants/Constants.java
@@ -1,7 +1,16 @@
 package com.connect.codeness.global.constants;
 
 public interface Constants {
+    // 페이징 상수
     String PAGE_SIZE = "10";
     String PAGE_NUMBER = "0";
+
+
+    //헤더 상수
     String AUTHORIZATION = "Authorization";
+
+
+    //멘토링 스케줄 상수
+    Long SCHEDULE_TIME = 1L;
+    Long RETENTION_DAYS = 30L;
 }
