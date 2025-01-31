991dabbb6f28accb96413411341451c7a64b397f
parkmookeun <94534687+parkmookeun@users.noreply.github.com>
2025-01-31 14:08:51 +0900

    🔧 Add : front config url 추가
    🔧 Add : front config url 추가

--- src/main/java/com/connect/codeness/global/config/SecurityConfig.java
+++ src/main/java/com/connect/codeness/global/config/SecurityConfig.java
@@ -26,6 +26,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+
 @Configuration
 public class SecurityConfig {
 
@@ -135,7 +136,9 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(List.of("http://localhost:3000"));  // 클라이언트의 실제 URL로 수정
+		configuration.setAllowedOrigins(List.of("http://localhost:3000",
+			"https://codeness-front.vercel.app/"
+			));  // 클라이언트의 실제 URL로 수정
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
 		configuration.setAllowedHeaders(List.of("*"));
 		configuration.setAllowCredentials(true);  // credentials 허용
